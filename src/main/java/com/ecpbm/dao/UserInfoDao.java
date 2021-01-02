0cb1b1bf5ab45f2ee98bf6f2cb76bf019e7e3972
kagura <1591934284@qq.com>
2021-01-02 16:29:39 +0800

    第三次提交-创建客户信息接口及对应动态SQL类

---
+++ src/main/java/com/ecpbm/dao/UserInfoDao.java
@@ -0,0 +1,45 @@
+package com.ecpbm.dao;
+
+import com.ecpbm.dao.provider.UserInfoDynaSqlProvider;
+import com.ecpbm.pojo.UserInfo;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @PACKAGE_NAME: com.ecpbm.dao
+ * @CLASS_NAME: UserInfoDao
+ * @USER: kagura
+ * @Description TODO 用户信息接口
+ * @Date 2021/1/2 3:49 下午
+ * @MONTH_NAME_FULL: 一月
+ * @DAY: 02
+ * @DAY_NAME_FULL: 星期六
+ * @PROJECT_NAME: ecpbm
+ **/
+public interface UserInfoDao {
+
+    // 获取系统合法用户，即数据表user_info中status字段为1的客户列表
+    @Select("select * from user_info t where t.status = 1")
+    public List<UserInfo> getValidUser();
+
+    // 根据客户id号获取客户对象
+    @Select("select * from user_info t where t where t.id = #{id}")
+    public List<UserInfo> getUserInfoById(Integer id);
+
+    // 分页获取客户信息
+    @SelectProvider(type = UserInfoDynaSqlProvider.class, method = "selectWithParam")
+    public List<UserInfo> selectWithParam(Map<String, Object> params);
+
+    // 根据条件查询客户总数
+    @SelectProvider(type = UserInfoDynaSqlProvider.class, method = "count")
+    Integer count(Map<String, Object> params);
+
+    // 更新客户状态
+    @Update("update user_info set status = #{flag} where id in (${ids})")
+    void updateState(@Param("ids") String ids, @Param("flag") Integer flag);
+}
---
+++ src/main/java/com/ecpbm/dao/provider/UserInfoDynaSqlProvider.java
@@ -0,0 +1,57 @@
+package com.ecpbm.dao.provider;
+
+import com.ecpbm.dao.UserInfoDao;
+import com.ecpbm.pojo.UserInfo;
+import org.apache.ibatis.jdbc.SQL;
+
+import java.util.Map;
+
+/**
+ * @PACKAGE_NAME: com.ecpbm.dao.provider
+ * @CLASS_NAME: UserInfoDynaSqlProvider
+ * @USER: kagura
+ * @Description TODO 用户信息动态SQL提供类
+ * @Date 2021/1/2 3:57 下午
+ * @MONTH_NAME_FULL: 一月
+ * @DAY: 02
+ * @DAY_NAME_FULL: 星期六
+ * @PROJECT_NAME: ecpbm
+ **/
+public class UserInfoDynaSqlProvider {
+
+    // 分页动态查询，Map<String, Object>类型的参数params用于封装查询条件
+    public String selectWithParam(final Map<String, Object> params) {
+        String sql = new SQL(){
+            {
+                SELECT("*");
+                FROM("user_info");
+                if (params.get("userInfo") != null) {
+                    UserInfo userInfo = (UserInfo) params.get("userInfo");
+                    if (userInfo.getUserName() != null && !userInfo.getUserName().equals("")) {
+                        WHERE("userName like concat('%', #{userInfo.userName}), '%'");
+                    }
+                }
+            }
+        }.toString();
+        if (params.get("pager") != null) {
+            sql += "limit #{pager.firstLimitParam}, #{pager.perPageRows}";
+        }
+        return sql;
+    }
+
+    // 根据条件动态查询总记录数
+    public String count(final Map<String, Object> params) {
+        return new SQL(){
+            {
+                SELECT("count(*)");
+                FROM("user_info");
+                if (params.get("userInfo") != null) {
+                    UserInfo userInfo = (UserInfo) params.get("userInfo");
+                    if (userInfo.getUserName() != null && !userInfo.getUserName().equals("")) {
+                        WHERE("userName like concat('%', #{userInfo,userName}, '%')");
+                    }
+                }
+            }
+        }.toString();
+    }
+}
