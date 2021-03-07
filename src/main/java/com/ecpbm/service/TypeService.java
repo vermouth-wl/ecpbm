070c4df805d00c8468de16743c93de3e30e87d3e
kagura <1591934284@qq.com>
2021-03-07 12:34:23 +0800

    第13次提交-创建TypeService接口

--- src/main/java/com/ecpbm/service/TypeService.java
+++ src/main/java/com/ecpbm/service/TypeService.java
@@ -1,10 +1,14 @@
 package com.ecpbm.service;
 
+import com.ecpbm.pojo.Type;
+
+import java.util.List;
+
 /**
  * @PACKAGE_NAME: com.ecpbm.service
  * @CLASS_NAME: TypeService
  * @USER: kagura
- * @Description TODO
+ * @Description TODO 商品类型信息业务逻辑接口
  * @Date 2021/1/17 7:10 下午
  * @MONTH_NAME_FULL: 一月
  * @DAY: 17
@@ -12,4 +16,13 @@ package com.ecpbm.service;
  * @PROJECT_NAME: ecpbm
  **/
 public interface TypeService {
+
+    // 获取所有商品类型
+    public List<Type> getAll();
+
+    // 添加商品类型
+    public int addType(Type type);
+
+    // 更新商品类型
+    public void updateType(Type type);
 }
