238848ccba1317e61011243ea14f26ec4cacd0cf
kagura <1591934284@qq.com>
2021-03-07 12:22:00 +0800

    第11次提交-创建AdminInfoService接口

--- src/main/java/com/ecpbm/service/AdminInfoService.java
+++ src/main/java/com/ecpbm/service/AdminInfoService.java
@@ -1,10 +1,12 @@
 package com.ecpbm.service;
 
+import com.ecpbm.pojo.AdminInfo;
+
 /**
  * @PACKAGE_NAME: com.ecpbm.service
  * @CLASS_NAME: AdminInfoService
  * @USER: kagura
- * @Description TODO
+ * @Description TODO 管理员信息业务逻辑接口
  * @Date 2021/1/17 7:10 下午
  * @MONTH_NAME_FULL: 一月
  * @DAY: 17
@@ -12,4 +14,10 @@ package com.ecpbm.service;
  * @PROJECT_NAME: ecpbm
  **/
 public interface AdminInfoService {
+
+    // 登录验证
+    public AdminInfo login(AdminInfo ai);
+
+    // 根据管理员编号，获取管理员对象及关联的功能权限
+    public AdminInfo getAdminInfoAndFunctions(Integer id);
 }
